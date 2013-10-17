245e6f9f3e75101f8058162b6d4670bd2814d5c7
Quentin Le Mesnager <quentin.le.mesnager@gmail.com>
2013-10-17 14:41:02 +0200

    update test_app

--- test/test_app.bat
+++ test/test_app.bat
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
 ### test_app.bat - 1.2.0-b - Le Mesnager
-=======
-### application.bat -201x - Le Mesnager
 
-version = "2.0.0"
-build_hour ="11H00"
 
-echo "Build${build_hour}"
-echo "version${version}"
->>>>>>> dev
